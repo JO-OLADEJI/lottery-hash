bc0df9906fa58fef5f6e023f5c10bbf57023b2f2
analogdev.eth <dejijolaoluwa@gmail.com>
2022-07-18 08:44:23 +0100

    fixed bugs responsible for failed synching

--- graph/src/mapping.ts
+++ graph/src/mapping.ts
@@ -3,12 +3,11 @@ import { Bytes } from "@graphprotocol/graph-ts";
 import {
   GameEnded,
   GameStarted,
-  OwnershipTransferred,
   PlayerJoined,
 } from "../generated/LotteryHash/LotteryHash";
 import { Game } from "../generated/schema";
 
-export const handleGameStarted = (event: GameStarted): void => {
+export function handleGameStarted(event: GameStarted): void {
   let entity = Game.load(event.params.gameId.toString());
   if (!entity) {
     entity = new Game(event.params.gameId.toString());
@@ -18,9 +17,9 @@ export const handleGameStarted = (event: GameStarted): void => {
   entity.entryFee = event.params.entryFee;
   entity.maxPlayers = event.params.maxPlayers;
   entity.save();
-};
+}
 
-export const handlePlayerJoined = (event: PlayerJoined): void => {
+export function handlePlayerJoined(event: PlayerJoined): void {
   const entity = Game.load(event.params.gameId.toString());
   if (!entity) return;
 
@@ -28,17 +27,13 @@ export const handlePlayerJoined = (event: PlayerJoined): void => {
   currentPlayers.push(event.params.player);
   entity.players = currentPlayers;
   entity.save();
-};
+}
 
-export const handleGameEnded = (event: GameEnded): void => {
+export function handleGameEnded(event: GameEnded): void {
   const entity = Game.load(event.params.gameId.toString());
   if (!entity) return;
 
   entity.winner = event.params.winner;
   entity.requestId = event.params.requestId;
   entity.save();
-};
-
-export const handleOwnershipTransferred = (
-  event: OwnershipTransferred
-): void => {};
+}
